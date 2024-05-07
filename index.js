c9b86df97a87f3307ecc4cbe3baf9374ddccd171
Gunski <159125111+Thaumagraph@users.noreply.github.com>
2024-05-07 10:55:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import http from 'http';
+import express from 'express'
+const app = express()
+const port = 3000
 
-const port = 3000;
-
-http.createServer((request, response) => {
-    console.log(`Webserver is running now on http://localhost:${port}`);
-	response.writeHead(200, { 'Content-Type': 'text/plain' });
-	response.write('Hello World');
-	response.end();
-}).listen(port)
+app.listen(port, () => {
+	console.log(
+		`webserver is running now on http://localhost:${port}`);
+	})
