aa515d66093c51b6fdf9a53b7e35836d5bbbf12c
Gunski <159125111+Thaumagraph@users.noreply.github.com>
2024-05-07 11:16:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.get('/', req, res) => {
+	res.send('Forside')}
+	
+
+app.get('/about', req, res) => {
+	res.send('Om os')}
+
 app.listen(port, () => {
 	console.log(
 		`webserver is running now on http://localhost:${port}`);
